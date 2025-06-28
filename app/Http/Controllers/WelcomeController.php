1a1c66ed9a02aa23de619d5ab1a9b51ca17e4130
titustum <tituskiptanuitum@gmail.com>
2025-06-28 23:34:30 +0300

    Better seeder

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -13,7 +13,7 @@ class WelcomeController extends Controller
 
         $categories = \App\Models\Category::take(8)->get();
         $products = \App\Models\Product::take(8)->get();
-        $topSellingProducts = Product::orderBy('price', 'desc')->take(8)->get();
+        $topSellingProducts = Product::inRandomOrder()->take(8)->get();
         $brands = Brand::limit(10)->get();  // Fetch top 10 brands
         return view('welcome', compact('categories', 'products', 'topSellingProducts', 'brands'));
     }
